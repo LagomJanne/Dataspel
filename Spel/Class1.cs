725412f98877517bcd72f67007827925fa7a2197
LagomJanne <126473450+LagomJanne@users.noreply.github.com>
2023-09-01 11:17:05 +0200

    Fortnite

---
+++ Spel/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Dataspel;
+
+
+namespace Dataspel
+{
+
+    class Dataspel
+    {
+        public string Spelarläge;
+        public string Genre;
+        public string Namn;
+    
+    public Dataspel(string ASpelarläge, string AGenre, string ANamn)
+        {
+            Spelarläge = ASpelarläge;
+            Genre = AGenre;
+            Namn = ANamn;
+        }
+
+    }
+}
---
+++ Spel/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Dataspel;
+
+
+namespace Dataspel
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gamesearched;
+
+            Dataspel Fortnite = new Dataspel("Multiplayer", "Shooter", "Fortnite");
+            Dataspel Ark = new Dataspel("Singleplayer", "Survival", "Ark");
+
+            Console.WriteLine("Search for a game: ");
+              gamesearched = Console.ReadLine();
+
+            foreach (Dataspel spel in new Dataspel[] { Fortnite, Ark })
+            {
+                if (gamesearched == spel.Namn)
+                {
+                    Console.WriteLine("\nName: " + spel.Namn + "\nGenre: " + spel.Genre + "\nSpelarläge: " + spel.Spelarläge);
+                }
+                else if (gamesearched != spel.Namn)
+                { 
+                    Console.Write(""); }
+            
+            }
+
+            
+        }
+    }
+
+
+
+}
